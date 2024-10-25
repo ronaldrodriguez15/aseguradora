d00dac26ab7c123bfde8fe88e77780f125fef444
ronaldrodriguez15 <ronalddanilo1234@gmail.com>
2024-10-25 11:17:44 -0500

    ajustes

--- public/js/reports.js
+++ public/js/reports.js
@@ -1,12 +1,44 @@
+$(document).ready(function () {
+    $("#datatable").DataTable({
+        columnDefs: [
+            {
+                defaultContent: "-",
+                targets: "_all",
+            },
+        ],
+        language: {
+            search: "Buscar",
+            emptyTable: "No hay datos disponibles en la tabla",
+            infoEmpty: "Mostrando 0 a 0 de 0 entradas",
+            lengthMenu: "Mostrar _MENU_ registros por página",
+            info: "Mostrando página _PAGE_ de _PAGES_",
+            zeroRecords: "No se encontraron registros coincidentes",
+            infoFiltered: "(filtrado de _MAX_ registros en total)",
+        },
+    });
+
+    $("#btnNuevaCiudad").click(function (e) {
+        e.preventDefault();
+        $("#modalNuevaCiudad").modal("show");
+    });
+
+    // Depurar el botón de cerrar
+    $(".close").click(function () {
+        $("#modalNuevaCiudad").modal("hide");
+    });
+});
+
 // Formatear la cédula mientras se escribe
-document.getElementById('cedula').addEventListener('input', function (e) {
-    let cedula = e.target.value.replace(/\D/g, ''); // Elimina todo lo que no sean dígitos
-    cedula = cedula.replace(/\B(?=(\d{3})+(?!\d))/g, '.'); // Añade los puntos
+document.getElementById("cedula").addEventListener("input", function (e) {
+    let cedula = e.target.value.replace(/\D/g, ""); // Elimina todo lo que no sean dígitos
+    cedula = cedula.replace(/\B(?=(\d{3})+(?!\d))/g, "."); // Añade los puntos
     e.target.value = cedula; // Actualiza el valor en el input
 });
 
 // Obtener el token CSRF desde el meta tag en la vista
-const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute("content");
+const csrfToken = document
+    .querySelector('meta[name="csrf-token"]')
+    .getAttribute("content");
 
 // Función para obtener los registros seleccionados
 function getSelectedRecords() {
@@ -19,207 +51,216 @@ function getSelectedRecords() {
 }
 
 // Manejar la selección de todos los checkboxes
-document.getElementById('select-all').addEventListener('change', function () {
-    const checkboxes = document.querySelectorAll('.record-checkbox');
-    checkboxes.forEach(checkbox => {
+document.getElementById("select-all").addEventListener("change", function () {
+    const checkboxes = document.querySelectorAll(".record-checkbox");
+    checkboxes.forEach((checkbox) => {
         checkbox.checked = this.checked;
         toggleButtons();
     });
 });
 
 // Verificar el estado de los checkboxes
-document.querySelectorAll('.record-checkbox').forEach(checkbox => {
-    checkbox.addEventListener('change', toggleButtons);
+document.querySelectorAll(".record-checkbox").forEach((checkbox) => {
+    checkbox.addEventListener("change", toggleButtons);
 });
 
 // Función para mostrar u ocultar los botones
 function toggleButtons() {
-    const checkboxes = document.querySelectorAll('.record-checkbox');
-    const isAnyChecked = Array.from(checkboxes).some(checkbox => checkbox.checked);
+    const checkboxes = document.querySelectorAll(".record-checkbox");
+    const isAnyChecked = Array.from(checkboxes).some(
+        (checkbox) => checkbox.checked
+    );
 
     // Mostrar u ocultar el contenedor de botones
-    document.getElementById('button-container').style.display = isAnyChecked ? 'block' : 'none';
+    document.getElementById("button-container").style.display = isAnyChecked
+        ? "block"
+        : "none";
 }
 
 // Inicialmente ocultar los botones
 toggleButtons();
 
 // Manejar el clic en el botón "Descargar PDFs"
-document.getElementById("descargar-pdfs").addEventListener("click", function (e) {
-    e.preventDefault();
+document
+    .getElementById("descargar-pdfs")
+    .addEventListener("click", function (e) {
+        e.preventDefault();
 
-    const selectedRecords = getSelectedRecords();
+        const selectedRecords = getSelectedRecords();
 
-    if (selectedRecords.length === 0) {
-        alert("No se ha seleccionado ningún registro.");
-        return;
-    }
+        if (selectedRecords.length === 0) {
+            alert("No se ha seleccionado ningún registro.");
+            return;
+        }
 
-    Swal.fire({
-        title: "Descargando PDFs",
-        text: "Por favor, espera mientras se genera tu ZIP...",
-        icon: "info",
-        allowOutsideClick: false,
-        showConfirmButton: false,
-        willOpen: () => Swal.showLoading(),
-    });
-
-    const form = document.createElement("form");
-    form.method = "POST";
-    form.action = "/descargar-pdfs";
-    form.style.display = "none";
-
-    const tokenInput = document.createElement("input");
-    tokenInput.type = "hidden";
-    tokenInput.name = "_token";
-    tokenInput.value = csrfToken;
-    form.appendChild(tokenInput);
-
-    const input = document.createElement("input");
-    input.type = "hidden";
-    input.name = "selected_records";
-    input.value = JSON.stringify(selectedRecords);
-    form.appendChild(input);
-
-    document.body.appendChild(form);
-    form.submit();
-
-    setTimeout(() => {
-        Swal.close();
         Swal.fire({
-            title: "Éxito!",
-            text: "ZIP descargado con éxito.",
-            icon: "success",
-            confirmButtonText: "Aceptar",
+            title: "Descargando PDFs",
+            text: "Por favor, espera mientras se genera tu ZIP...",
+            icon: "info",
+            allowOutsideClick: false,
+            showConfirmButton: false,
+            willOpen: () => Swal.showLoading(),
         });
-    }, 1000);
-});
+
+        const form = document.createElement("form");
+        form.method = "POST";
+        form.action = "/descargar-pdfs";
+        form.style.display = "none";
+
+        const tokenInput = document.createElement("input");
+        tokenInput.type = "hidden";
+        tokenInput.name = "_token";
+        tokenInput.value = csrfToken;
+        form.appendChild(tokenInput);
+
+        const input = document.createElement("input");
+        input.type = "hidden";
+        input.name = "selected_records";
+        input.value = JSON.stringify(selectedRecords);
+        form.appendChild(input);
+
+        document.body.appendChild(form);
+        form.submit();
+
+        setTimeout(() => {
+            Swal.close();
+            Swal.fire({
+                title: "Éxito!",
+                text: "ZIP descargado con éxito.",
+                icon: "success",
+                confirmButtonText: "Aceptar",
+            });
+        }, 1000);
+    });
 
 // Manejar el clic en el botón "Descargar Plano Focus"
-document.getElementById("descargar-plano").addEventListener("click", function (e) {
-    e.preventDefault();
+document
+    .getElementById("descargar-plano")
+    .addEventListener("click", function (e) {
+        e.preventDefault();
 
-    const selectedRecords = getSelectedRecords();
+        const selectedRecords = getSelectedRecords();
 
-    if (selectedRecords.length === 0) {
-        alert("No se ha seleccionado ningún registro.");
-        return;
-    }
+        if (selectedRecords.length === 0) {
+            alert("No se ha seleccionado ningún registro.");
+            return;
+        }
 
-    Swal.fire({
-        title: "Descargando Excel",
-        text: "Por favor, espera mientras se genera tu archivo...",
-        icon: "info",
-        allowOutsideClick: false,
-        showConfirmButton: false,
-        willOpen: () => Swal.showLoading(),
-    });
-
-    const form = new FormData();
-    form.append("_token", csrfToken);
-    form.append("selected_records", JSON.stringify(selectedRecords));
-
-    fetch("/descargar-plano-focus", {
-        method: "POST",
-        body: form,
-    })
-        .then((response) => {
-            if (response.ok) {
-                return response.blob();
-            }
-            throw new Error("Network response was not ok.");
-        })
-        .then((blob) => {
-            const url = window.URL.createObjectURL(blob);
-            const a = document.createElement("a");
-            a.href = url;
-            a.download = `resultado_${Date.now()}.xlsx`;
-            document.body.appendChild(a);
-            a.click();
-            a.remove();
-            window.URL.revokeObjectURL(url);
-
-            Swal.close();
-            Swal.fire({
-                title: "Éxito!",
-                text: "Excel descargado con éxito.",
-                icon: "success",
-                confirmButtonText: "OK",
-            });
-        })
-        .catch((error) => {
-            console.error("Error:", error);
-            Swal.close();
-            Swal.fire({
-                title: "Error!",
-                text: "Hubo un problema al procesar la solicitud.",
-                icon: "error",
-                confirmButtonText: "OK",
-            });
+        Swal.fire({
+            title: "Descargando Excel",
+            text: "Por favor, espera mientras se genera tu archivo...",
+            icon: "info",
+            allowOutsideClick: false,
+            showConfirmButton: false,
+            willOpen: () => Swal.showLoading(),
         });
-});
+
+        const form = new FormData();
+        form.append("_token", csrfToken);
+        form.append("selected_records", JSON.stringify(selectedRecords));
+
+        fetch("/descargar-plano-focus", {
+            method: "POST",
+            body: form,
+        })
+            .then((response) => {
+                if (response.ok) {
+                    return response.blob();
+                }
+                throw new Error("Network response was not ok.");
+            })
+            .then((blob) => {
+                const url = window.URL.createObjectURL(blob);
+                const a = document.createElement("a");
+                a.href = url;
+                a.download = `resultado_${Date.now()}.xlsx`;
+                document.body.appendChild(a);
+                a.click();
+                a.remove();
+                window.URL.revokeObjectURL(url);
+
+                Swal.close();
+                Swal.fire({
+                    title: "Éxito!",
+                    text: "Excel descargado con éxito.",
+                    icon: "success",
+                    confirmButtonText: "OK",
+                });
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+                Swal.close();
+                Swal.fire({
+                    title: "Error!",
+                    text: "Hubo un problema al procesar la solicitud.",
+                    icon: "error",
+                    confirmButtonText: "OK",
+                });
+            });
+    });
 
 // Manejar el clic en el botón "Descargar Seguimiento"
-document.getElementById("descargar-seguimiento").addEventListener("click", function (e) {
-    e.preventDefault();
+document
+    .getElementById("descargar-seguimiento")
+    .addEventListener("click", function (e) {
+        e.preventDefault();
 
-    const selectedRecords = getSelectedRecords();
+        const selectedRecords = getSelectedRecords();
 
-    if (selectedRecords.length === 0) {
-        alert("No se ha seleccionado ningún registro.");
-        return;
-    }
+        if (selectedRecords.length === 0) {
+            alert("No se ha seleccionado ningún registro.");
+            return;
+        }
 
-    Swal.fire({
-        title: "Descargando Excel",
-        text: "Por favor, espera mientras se genera tu archivo...",
-        icon: "info",
-        allowOutsideClick: false,
-        showConfirmButton: false,
-        willOpen: () => Swal.showLoading(),
-    });
-
-    const form = new FormData();
-    form.append("_token", csrfToken);
-    form.append("selected_records", JSON.stringify(selectedRecords));
-
-    fetch("/descargar-seguimiento-ventas", {
-        method: "POST",
-        body: form,
-    })
-        .then((response) => {
-            if (response.ok) {
-                return response.blob();
-            }
-            throw new Error("Network response was not ok.");
-        })
-        .then((blob) => {
-            const url = window.URL.createObjectURL(blob);
-            const a = document.createElement("a");
-            a.href = url;
-            a.download = `resultado_${Date.now()}.xlsx`;
-            document.body.appendChild(a);
-            a.click();
-            a.remove();
-            window.URL.revokeObjectURL(url);
-
-            Swal.close();
-            Swal.fire({
-                title: "Éxito!",
-                text: "Excel descargado con éxito.",
-                icon: "success",
-                confirmButtonText: "OK",
-            });
-        })
-        .catch((error) => {
-            console.error("Error:", error);
-            Swal.close();
-            Swal.fire({
-                title: "Error!",
-                text: "Hubo un problema al procesar la solicitud.",
-                icon: "error",
-                confirmButtonText: "OK",
-            });
+        Swal.fire({
+            title: "Descargando Excel",
+            text: "Por favor, espera mientras se genera tu archivo...",
+            icon: "info",
+            allowOutsideClick: false,
+            showConfirmButton: false,
+            willOpen: () => Swal.showLoading(),
         });
-});
 
+        const form = new FormData();
+        form.append("_token", csrfToken);
+        form.append("selected_records", JSON.stringify(selectedRecords));
+
+        fetch("/descargar-seguimiento-ventas", {
+            method: "POST",
+            body: form,
+        })
+            .then((response) => {
+                if (response.ok) {
+                    return response.blob();
+                }
+                throw new Error("Network response was not ok.");
+            })
+            .then((blob) => {
+                const url = window.URL.createObjectURL(blob);
+                const a = document.createElement("a");
+                a.href = url;
+                a.download = `resultado_${Date.now()}.xlsx`;
+                document.body.appendChild(a);
+                a.click();
+                a.remove();
+                window.URL.revokeObjectURL(url);
+
+                Swal.close();
+                Swal.fire({
+                    title: "Éxito!",
+                    text: "Excel descargado con éxito.",
+                    icon: "success",
+                    confirmButtonText: "OK",
+                });
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+                Swal.close();
+                Swal.fire({
+                    title: "Error!",
+                    text: "Hubo un problema al procesar la solicitud.",
+                    icon: "error",
+                    confirmButtonText: "OK",
+                });
+            });
+    });
